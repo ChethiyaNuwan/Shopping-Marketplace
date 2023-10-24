97cd64598aaa627b4fb5f75749fa4885e060faf1
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-25 02:31:04 +0530

    created wishlist controller

--- src/main/java/com/chethiya/shopping_marketplace/controllers/WishlistController.java
+++ src/main/java/com/chethiya/shopping_marketplace/controllers/WishlistController.java
@@ -1,7 +1,32 @@
 package com.chethiya.shopping_marketplace.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.chethiya.shopping_marketplace.models.Wishlist;
+import com.chethiya.shopping_marketplace.services.WishlistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class WishlistController {
+
+    @Autowired
+    private WishlistService wishlistService;
+
+    @PostMapping("/products/{productId}/wishlist")
+    @ResponseBody
+    public ResponseEntity<Object> addToWishlist(
+            @PathVariable String productId,
+            @RequestParam String userId
+    ) {
+         return wishlistService.addToWishlist(productId, userId);
+    }
+
+    @GetMapping("/users/{userId}/wishlist")
+    @ResponseBody
+    public ResponseEntity<Object> getWishlistByUserId(
+            @PathVariable String userId
+    ) {
+        return wishlistService.getWishlistByUserId(userId);
+    }
 }
