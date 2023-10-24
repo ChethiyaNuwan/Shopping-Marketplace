3819895f4e348786f28c5b98fc9eb702498f1e52
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-24 21:24:21 +0530

    created user controller

---
+++ src/main/java/com/chethiya/shopping_marketplace/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.chethiya.shopping_marketplace.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+
+}
