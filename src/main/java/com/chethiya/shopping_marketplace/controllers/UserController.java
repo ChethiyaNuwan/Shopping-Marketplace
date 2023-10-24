5893bc19af1353d3495a94c016b6cd21a1071f38
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-24 21:24:44 +0530

    added post mapping to user controller

--- src/main/java/com/chethiya/shopping_marketplace/controllers/UserController.java
+++ src/main/java/com/chethiya/shopping_marketplace/controllers/UserController.java
@@ -1,10 +1,21 @@
 package com.chethiya.shopping_marketplace.controllers;
 
+import com.chethiya.shopping_marketplace.models.User;
+import com.chethiya.shopping_marketplace.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
 
+    @PostMapping("")
+    @ResponseBody
+    public ResponseEntity<Object> createUser(@RequestBody User user){
+        return userService.createUser(user);
+    }
 }
