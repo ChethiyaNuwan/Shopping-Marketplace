525d20a61fc99444d6a0582943a1b58f495ea3fc
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-25 02:34:24 +0530

    created product controller

--- src/main/java/com/chethiya/shopping_marketplace/controllers/ProductController.java
+++ src/main/java/com/chethiya/shopping_marketplace/controllers/ProductController.java
@@ -1,7 +1,32 @@
 package com.chethiya.shopping_marketplace.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.chethiya.shopping_marketplace.models.Product;
+import com.chethiya.shopping_marketplace.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("products")
+    @ResponseBody
+    public ResponseEntity<Object> addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+
+    @GetMapping("products")
+    @ResponseBody
+    public ResponseEntity<Object> getAllProducts(
+            @RequestParam(value = "category", required = false, defaultValue = "") String category,
+            @RequestParam(value = "minPrice", required = false, defaultValue = "") Double minPrice,
+            @RequestParam(value = "maxPrice", required = false, defaultValue = "") Double maxPrice,
+            @RequestParam(value = "sortBy", required = false, defaultValue = "") String sortBy,
+            @RequestParam(value = "order", required = false, defaultValue = "") String order
+    ) {
+        return productService.getAllProducts(category, minPrice, maxPrice, sortBy, order);
+    }
 }
