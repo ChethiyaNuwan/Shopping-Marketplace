77948fbd84c4488b0ee37c713c9a91d65dc4263f
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-24 21:26:07 +0530

    defined create user functionality

--- src/main/java/com/chethiya/shopping_marketplace/services/UserService.java
+++ src/main/java/com/chethiya/shopping_marketplace/services/UserService.java
@@ -1,8 +1,18 @@
 package com.chethiya.shopping_marketplace.services;
 
+import com.chethiya.shopping_marketplace.models.User;
+import com.chethiya.shopping_marketplace.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    public ResponseEntity<Object> createUser(User user) {
+        return ResponseEntity.status(201).body(userRepository.save(user));
+    }
 }
