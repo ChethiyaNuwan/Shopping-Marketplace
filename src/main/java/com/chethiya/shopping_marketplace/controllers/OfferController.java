344d65b51409cff8bf8bf80f493aae643f0c699c
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-25 02:26:02 +0530

    created offer controller

--- src/main/java/com/chethiya/shopping_marketplace/controllers/OfferController.java
+++ src/main/java/com/chethiya/shopping_marketplace/controllers/OfferController.java
@@ -1,12 +1,31 @@
 package com.chethiya.shopping_marketplace.controllers;
 
+import com.chethiya.shopping_marketplace.models.Offer;
 import com.chethiya.shopping_marketplace.services.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class OfferController {
 
     @Autowired
     private OfferService offerService;
+
+    @PostMapping("/products/{productId}/offer")
+    @ResponseBody
+    public ResponseEntity<Object> makeOffer(
+            @PathVariable String productId,
+            @RequestBody Offer offer
+    ) {
+        return offerService.makeOffer(productId, offer);
+    }
+
+    @GetMapping("/users/{userId}/offers")
+    @ResponseBody
+    public ResponseEntity<Object> getOffersByUserId(
+            @PathVariable String userId
+    ) {
+        return offerService.getOffersByUserId(userId);
+    }
 }
