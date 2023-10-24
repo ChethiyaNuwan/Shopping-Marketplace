b518c884fb514b9b79246815ef375238d1ccd020
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-25 02:28:05 +0530

    created order controller

--- src/main/java/com/chethiya/shopping_marketplace/controllers/OrderController.java
+++ src/main/java/com/chethiya/shopping_marketplace/controllers/OrderController.java
@@ -1,8 +1,31 @@
 package com.chethiya.shopping_marketplace.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.chethiya.shopping_marketplace.models.Order;
+import com.chethiya.shopping_marketplace.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class OrderController {
 
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/products/{productId}/order")
+    @ResponseBody
+    public ResponseEntity<Object> orderProduct(
+            @PathVariable String productId,
+            @RequestBody Order order
+    ) {
+        return orderService.orderProduct(productId, order);
+    }
+
+    @GetMapping("/users/{userId}/orders")
+    @ResponseBody
+    public ResponseEntity<Object> getOrdersByUserId(
+            @PathVariable String userId
+    ) {
+        return orderService.getOrdersByUserId(userId);
+    }
 }
