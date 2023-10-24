a5c877969b695563f7b4cb36f423cbf29ee63b1b
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-25 02:33:42 +0530

    added query to find wishlist by user id

--- src/main/java/com/chethiya/shopping_marketplace/repositories/WishlistRepository.java
+++ src/main/java/com/chethiya/shopping_marketplace/repositories/WishlistRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishlistRepository extends MongoRepository<Wishlist,String> {
+    Wishlist findByUserId(String userId);
 }
