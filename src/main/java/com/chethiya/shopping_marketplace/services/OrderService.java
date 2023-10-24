eba115bf0c2d295057789227357692e0739c5e7b
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-25 02:28:43 +0530

    defined order product and get orders by user id

--- src/main/java/com/chethiya/shopping_marketplace/services/OrderService.java
+++ src/main/java/com/chethiya/shopping_marketplace/services/OrderService.java
@@ -1,7 +1,28 @@
 package com.chethiya.shopping_marketplace.services;
 
+import com.chethiya.shopping_marketplace.models.Order;
+import com.chethiya.shopping_marketplace.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public ResponseEntity<Object> orderProduct(String productId, Order order) {
+        order.setProductId(productId);
+
+        Order savedOrder = orderRepository.save(order);
+        return ResponseEntity.status(201).body(savedOrder);
+    }
+
+    public ResponseEntity<Object> getOrdersByUserId(String userId) {
+        List<Order> orders = orderRepository.findByUserId(userId);
+        return ResponseEntity.status(200).body(orders);
+    }
 }
