026f04e3c03e1c26011c8c031e731988d69a3327
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-25 00:00:44 +0530

    added object id generation for id

--- src/main/java/com/chethiya/shopping_marketplace/services/UserService.java
+++ src/main/java/com/chethiya/shopping_marketplace/services/UserService.java
@@ -2,6 +2,7 @@ package com.chethiya.shopping_marketplace.services;
 
 import com.chethiya.shopping_marketplace.models.User;
 import com.chethiya.shopping_marketplace.repositories.UserRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,8 @@ public class UserService {
                     .status(409)
                     .body("User with email already exists");
         }
+
+        user.setId(new ObjectId().toString());
         return ResponseEntity
                 .status(201)
                 .body(userRepository.save(user));
