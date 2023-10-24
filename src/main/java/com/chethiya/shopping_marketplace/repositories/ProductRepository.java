9b4f9e5ecfd2787b9fcc4606a27f7540d08805b1
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-25 04:05:31 +0530

    developed query to filter out data dynamically

--- src/main/java/com/chethiya/shopping_marketplace/repositories/ProductRepository.java
+++ src/main/java/com/chethiya/shopping_marketplace/repositories/ProductRepository.java
@@ -1,9 +1,22 @@
 package com.chethiya.shopping_marketplace.repositories;
 
 import com.chethiya.shopping_marketplace.models.Product;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
 @Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
+    List<Product> findByCategoryAndPriceBetween(String category, Double min, Double max);
+    @Aggregation(
+            pipeline = {
+                    "{ $match: { category: { $regex: ?0, $options: 'i' } } }",
+                    "{ $match: { $and: [ { price: { $gte: ?1 } }, { price: { $lte: ?2 } } ] } }",
+                    "{ $sort: { ?3: ?4 } }"
+            }
+    )
+    List<Product> findAllByFilters(String category, Double minPrice, Double maxPrice, String sortBy, int order);
 }
