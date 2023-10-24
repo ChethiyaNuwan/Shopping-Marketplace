b265bc642bfc5a03eba0159fc51b8792fa7ebd59
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-24 21:52:54 +0530

    created product model

---
+++ src/main/java/com/chethiya/shopping_marketplace/models/Product.java
@@ -0,0 +1,22 @@
+package com.chethiya.shopping_marketplace.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+    private String sellerId;
+}
