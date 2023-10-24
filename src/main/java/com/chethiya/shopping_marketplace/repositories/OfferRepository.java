2adbe9acee1626df0b7d36d871ebc1cb9b7a1241
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-25 02:27:27 +0530

    added find by user id query

--- src/main/java/com/chethiya/shopping_marketplace/repositories/OfferRepository.java
+++ src/main/java/com/chethiya/shopping_marketplace/repositories/OfferRepository.java
@@ -4,6 +4,9 @@ import com.chethiya.shopping_marketplace.models.Offer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OfferRepository extends MongoRepository<Offer,String> {
+    List<Offer> findByUserId(String userId);
 }
