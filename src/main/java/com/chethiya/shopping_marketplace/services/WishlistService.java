81329aec7ce2ad28ef9feb87d8d7d0865b5af271
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-25 02:33:10 +0530

    defined add to wishlist and get wishlists user id functions

--- src/main/java/com/chethiya/shopping_marketplace/services/WishlistService.java
+++ src/main/java/com/chethiya/shopping_marketplace/services/WishlistService.java
@@ -1,7 +1,45 @@
 package com.chethiya.shopping_marketplace.services;
 
+import com.chethiya.shopping_marketplace.models.Wishlist;
+import com.chethiya.shopping_marketplace.repositories.WishlistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class WishlistService {
+
+    @Autowired
+    private WishlistRepository wishlistRepository;
+
+    public ResponseEntity<Object> addToWishlist(String productId, String userId) {
+        Wishlist wishlist = wishlistRepository.findByUserId(userId);
+        if (wishlist == null) {
+            wishlist = new Wishlist();
+            wishlist.setUserId(userId);
+            wishlist.setProductIds(new ArrayList<>());
+        }
+
+        List<String> productIds = wishlist.getProductIds();
+        if (!productIds.contains(productId)) {
+            productIds.add(productId);
+            wishlistRepository.save(wishlist);
+            return ResponseEntity.status(201).body("Product added to the wishlist successfully.");
+        }
+
+        return ResponseEntity.status(400).body("Product is already in the wishlist.");
+    }
+
+    public ResponseEntity<Object> getWishlistByUserId(String userId) {
+        Wishlist wishlist = wishlistRepository.findByUserId(userId);
+        if (wishlist != null) {
+            List<String> productIds = wishlist.getProductIds();
+            return ResponseEntity.status(200).body(productIds);
+        }
+
+        return ResponseEntity.status(404).body("Wishlist not found for the user.");
+    }
 }
