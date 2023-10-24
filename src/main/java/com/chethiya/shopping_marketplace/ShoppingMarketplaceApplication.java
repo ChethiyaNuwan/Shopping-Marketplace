f5498260119922bf2d79e8e9f406484f306304ed
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-24 14:12:00 +0530

    Initial commit: project created

---
+++ src/main/java/com/chethiya/shopping_marketplace/ShoppingMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.chethiya.shopping_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chethiya/shopping_marketplace/ShoppingMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chethiya.shopping_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
