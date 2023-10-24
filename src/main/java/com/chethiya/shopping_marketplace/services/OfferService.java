082aff92ea1a4edc54a079cc3eb867196e56c994
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-10-25 02:27:04 +0530

    defined make offer and get offers by user id functions

--- src/main/java/com/chethiya/shopping_marketplace/services/OfferService.java
+++ src/main/java/com/chethiya/shopping_marketplace/services/OfferService.java
@@ -1,8 +1,28 @@
 package com.chethiya.shopping_marketplace.services;
 
+import com.chethiya.shopping_marketplace.models.Offer;
+import com.chethiya.shopping_marketplace.repositories.OfferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OfferService {
 
+    @Autowired
+    private OfferRepository offerRepository;
+
+    public ResponseEntity<Object> makeOffer(String productId, Offer offer) {
+        offer.setProductId(productId);
+
+        Offer savedOffer = offerRepository.save(offer);
+        return ResponseEntity.status(201).body(savedOffer);
+    }
+
+    public ResponseEntity<Object> getOffersByUserId(String userId) {
+        List<Offer> offers = offerRepository.findByUserId(userId);
+        return ResponseEntity.status(200).body(offers);
+    }
 }
